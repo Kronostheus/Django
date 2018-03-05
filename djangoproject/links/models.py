1338ad6efd591260457204935d3003dd8df97860
Kronostheus <nuno.amaro101@gmail.com>
2018-03-05 01:26:01 +0000

    initial commit

---
+++ djangoproject/links/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Link(models.Model):
+    title = models.CharField("Headline", max_length=100)
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    rank = models.FloatField(default = 0.0)
+    url = models.URLField("URL", max_length=250, blank=True)
+    description = models.TextField(blank=True)
+
+    def __srt__(self):
+        return self.title
+
+class Vote(models.Model):
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s upvoted %s" % (self.voter.username, self.link.title)
