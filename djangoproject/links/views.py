750c99395983f0440d75a9cb28f0cf1aca4ed9f1
Kronostheus <nuno.amaro101@gmail.com>
2018-03-05 13:02:33 +0000

    Reorder templates/ and signup form

--- djangoproject/links/views.py
+++ djangoproject/links/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Link, Vote
 
 # Create your views here.
 def index(request):
-    return render(request, 'links/html')
+    links = Link.objects.all()[:10]
+
+    context = {
+        'links': links
+    }
+
+    return render(request, 'links/index.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
